320aa6ecfd5f50a9bac51d905f242bdb33e5bd66
Dakota <99370868+WhoisDakota@users.noreply.github.com>
2022-12-05 14:07:55 -0700

    resolved read/write issues
    added array to store data. worked read_fn(), write_fun(), added comments. works with make->chmod->running simple echo "Hello world" > process_generator. stores data and can read it back out.

--- proc_filesys.c
+++ proc_filesys.c
@@ -12,34 +12,47 @@ MODULE_AUTHOR(AUTHOR);
 
 static int uuid = 0;
 module_param(uuid, int, 0);
+#define MAX_SIZE 100;
+static char data[MAX_SIZE];
 
 
 static struct proc_dir_entry *test_proc_entry;
 static struct proc_dir_entry *test_proc_entry2;
 static struct proc_dir_entry *proc_parent;
 
+
 static ssize_t read_fn(struct file *file, char __user *ubuf, size_t count, loff_t *ppos);
 static ssize_t write_fn(struct file *file, const char __user *ubuf, size_t count, loff_t *ppos);
 
 
+/*function pointers to funcitons needed to be called when a read/write is called on specific file*/
 static struct proc_ops test_procfs = {
 	.proc_read = read_fn,
 	.proc_write = write_fn,
 };
 
+//store data read in array 'data'
 static ssize_t read_fn(struct file *file, char __user *ubuf, size_t count, loff_t *ppos){
 
-	return count;
+	int length = 0;
+	length = sprintf(ubuf, "\n %s\n ", data);
+	return length;
+
 }
 
+//TODO- not sure how to accept data from user and put into array called 'data'
 static ssize_t write_fn(struct file *file, const char __user *ubuf, size_t count, loff_t *ppos){
+	if(count > MAX_SIZE){
+		count = MAX_SIZE;
+	}
 	
 	return count;
 }
 
 
+
 void create_new_proc_entry(void){
-	proc_parent = proc_mkdir("cse330", NULL);
+	proc_parent = proc_mkdir("cse330", NULL);	//create new directory 
 	test_proc_entry = proc_create("identity", 0666, proc_parent, &test_procfs);
 	test_proc_entry2 = proc_create("statistics", 0666, proc_parent, &test_procfs);
 	
